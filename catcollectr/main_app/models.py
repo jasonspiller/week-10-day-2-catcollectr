45b99fc149ad5913d9a9bd0ac7aad60665cea01e
Jason Spiller <jason@thetargetlab.com>
2018-05-22 15:05:12 -0600

    Using models.

--- catcollectr/main_app/models.py
+++ catcollectr/main_app/models.py
@@ -1,3 +1,15 @@
+"""Models."""
 from django.db import models
 
-# Create your models here.
+
+class Cat(models.Model):
+    """Cat class."""
+
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    age = models.IntegerField()
+
+    def __str__(self):
+        """Output name."""
+        return self.name
--- catcollectr/main_app/views.py
+++ catcollectr/main_app/views.py
@@ -1,26 +1,10 @@
 """Views."""
 from django.shortcuts import render
+from .models import Cat
 
 
 # Create your views here.
 def index(request):
     """Index page."""
+    cats = Cat.objects.all()
     return render(request, 'index.html', {'cats': cats})
-
-
-class Cat:
-    """Cat class."""
-
-    def __init__(self, name, breed, description, age):
-        """Cat class constructor."""
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-
-cats = [
-    Cat('Lolo', 'tabby', 'foul little demon', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
