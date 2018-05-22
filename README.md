afba263495b06cd893703042ff639195e8bf60d1
Jason Spiller <jason@thetargetlab.com>
2018-05-22 13:54:20 -0600

    Template working.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Typo:
 
+Readme.md
+===
 5. Now we will map this particular view to a url. We want to use the route /index for now as an example. Lets add a url for our view in the url dispatcher file in CatCollector/CatCollectr/urls.py:
 
 urlpatterns = [
@@ -9,3 +11,9 @@ urlpatterns = [
 	# add the line below to your urlpatterns array
 	path('**index/**', views.index)
 ]
+
+Showing Data
+---
+Try to limit horizontal scrolling on code snippets (looks like there may be a formatting issue)
+
+3. formatting issue
--- catcollectr/main_app/views.py
+++ catcollectr/main_app/views.py
@@ -1,8 +1,26 @@
 """Views."""
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
-def index(self):
+def index(request):
     """Index page."""
-    return HttpResponse('<h1>Hello World! /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'index.html', {'cats': cats})
+
+
+class Cat:
+    """Cat class."""
+
+    def __init__(self, name, breed, description, age):
+        """Cat class constructor."""
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+
+cats = [
+    Cat('Lolo', 'tabby', 'foul little demon', 3),
+    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
